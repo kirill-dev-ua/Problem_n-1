2303c89c23b9852f8e2770ce8f80ee0b698ef03d
observerUA <kirillkuznetsov181@gmail.com>
2025-07-05 19:08:27 +0300

    self inject or moving the logic to another class

--- src/main/java/school/n1problem/service/ClientService2.java
+++ src/main/java/school/n1problem/service/ClientService2.java
@@ -2,6 +2,7 @@ package school.n1problem.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import school.n1problem.db.ClientRepository;
 import school.n1problem.model.Client;
@@ -12,12 +13,23 @@ public class ClientService2 {
 
     private final ClientRepository repository;
     private final ClientService3 clientService3;
+    private final ClientService2 clientService2;
 
     @Transactional(noRollbackFor = RuntimeException.class)
     public void outerWithRollbackTest() {
         Client a = new Client();
         a.setName("Client A");
         repository.save(a);
+//        innerNotSupported();
+//        clientService2.innerNotSupported();
         clientService3.innerNotSupported();
     }
+
+//    @Transactional(propagation = Propagation.REQUIRES_NEW)
+//    public void innerNotSupported() {
+//        Client b = new Client();
+//        b.setName("Client B");
+//        repository.save(b);
+//        throw new RuntimeException("Error");
+//    }
 }
